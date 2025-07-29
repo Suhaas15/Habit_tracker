5e20fae69eaab70565a3088b5f9fb401ff25075c
Suhaas15 <suhaas15032001@gmail.com>
2025-07-29 16:56:58 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,43 @@
+import requests
+from datetime import datetime
+
+pixela_endpoint="https://pixe.la/v1/users"
+
+TOKEN="token"
+USERNAME="name"
+GRAPH_ID = "graph_id"
+
+user_params={
+    "token": TOKEN,
+    "username": USERNAME,
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes",
+}
+
+# response = requests.post(url=pixela_endpoint, json=user_params)       #https://pixe.la/@suhaas15
+# print(response.text)
+
+graph_endpoint=f"{pixela_endpoint}/{USERNAME}/graphs"
+graph_config={
+    "id": GRAPH_ID,
+    "name": "Coding graph",
+    "unit": "questions",
+    "type": "int",
+    "color": "ajisai",
+}
+headers = {
+    "X-USER-TOKEN": TOKEN,
+}
+
+# response=requests.post(url=graph_endpoint, json=graph_config, headers=headers)        #https://pixe.la/v1/users/suhaas15/graphs/graph1.html
+# print(response.text)
+
+pixel_creation_endpoint=f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}"
+today=datetime.now().strftime("%Y%m%d")
+pixel_data={
+    "date": today,
+    "quantity": input("How many coding problems did you solve today?"),
+}
+
+response=requests.post(url=pixel_creation_endpoint, headers=headers, json=pixel_data)
+print(response.text)
